c23b6fb9a19a8f5020adf8904dbfd44a8f230f93
Matthew Ault <matthew.ault@gmail.com>
2018-08-16 14:36:11 -0700

    rm aws env from container, this should be handled via roles

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker run -p 8080:8080/tcp -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY aultimus/shortly:latest
+sudo docker run -p 8080:8080/tcp aultimus/shortly:latest
 
 #sudo docker compose up
 # for debugging
