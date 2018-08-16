46aeda6f38011b22a2a464dae10e90fbc2d301f1
Matthew Ault <matthew.ault@gmail.com>
2018-08-16 14:11:07 -0700

    docker: expose port and setup aws access

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#sudo docker run aultimus/shortly:latest
-sudo docker compose up
+sudo docker run -p 8080:8080/tcp -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY aultimus/shortly:latest
+
+#sudo docker compose up
 # for debugging
 # sudo docker run -it --entrypoint /bin/sh aultimus/shortly:latest
 # sudo docker exec -i -t $CONTAINER_ID /bin/bash
