90b4c9b7ef900dea013c33fc898177757905d68d
Matthew Ault <matthew.ault@gmail.com>
2018-08-17 15:58:27 -0700

    run: fix hanging dynamo requests, provide env vars to container
    the env vars are entered into eb / aws itself.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker run -p 8080:8080/tcp 361313012007.dkr.ecr.us-west-2.amazonaws.com/shortly:latest
+sudo docker run -p 8080:8080/tcp --env-file .env.local 361313012007.dkr.ecr.us-west-2.amazonaws.com/shortly:latest
 
 #sudo docker compose up
 # for debugging
