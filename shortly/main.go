6c3bf05bae4cb6608bda066a846af64ebf476162
Matthew Ault <matthew.ault@gmail.com>
2018-08-17 17:46:58 -0700

    pprof: add pprof to help debug hanging dynamodb calls

--- shortly/main.go
+++ shortly/main.go
@@ -6,11 +6,19 @@ import (
 
 	"github.com/aultimus/shortly/db"
 
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/aultimus/shortly"
 )
 
 func main() {
 	fmt.Println("shortly started")
+
+	go func() {
+		log.Println(http.ListenAndServe(":6060", nil))
+	}()
+
 	app := shortly.NewApp()
 	err := app.Init(db.NewDynamoService())
 	if err != nil {
