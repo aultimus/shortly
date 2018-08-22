2ec533071cb84a3249e2f834fa3f572a042daed1
Matthew Ault <matthew.ault@gmail.com>
2018-08-21 19:02:00 -0700

    tag docker image with git sha as well as latest tag

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ SHA=`git rev-parse --short HEAD`
 CGO_ENABLED=0 GOOS=linux go build -a -ldflags "-s -X main.gitSHA=$SHA -extldflags '-static'" .
 chmod +x shortly
 popd
-sudo docker build -t 361313012007.dkr.ecr.us-west-2.amazonaws.com/shortly:latest .
+sudo docker build -t 361313012007.dkr.ecr.us-west-2.amazonaws.com/shortly:latest -t 361313012007.dkr.ecr.us-west-2.amazonaws.com/shortly:$SHA .
--- push.sh
+++ push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo $(aws ecr get-login --no-include-email --region us-west-2)
-sudo docker push 361313012007.dkr.ecr.us-west-2.amazonaws.com/shortly:latest
+sudo docker push 361313012007.dkr.ecr.us-west-2.amazonaws.com/shortly
